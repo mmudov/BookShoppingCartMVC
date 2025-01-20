a6ab800012cc698842011d51d626ea4ecbbcf75f
mmudov <milen.mudov@gmail.com>
2025-01-20 15:00:05 +0200

    - Creation of ASP.NET Core Web App (MVC) project
    - "DefaultConnection" setting
    - Add "New Scaffold Item..." -> "Identity"
    - Creation of BookShoppingCartMVC\BookShoppingCartMVC\Models: \Book.cs; \CartDetail; \Genre; \Order; \OrderDetail; \OrderStatus; \ShoppingCart; \Stock
    - Add "Books", "CartDetails", "Genres", "Orders", "OrderDetails", "OrderStatuses", "ShoppingCarts", "Stocks" to BookShoppingCartMVC\BookShoppingCartMVC\Data\ApplicationDbContext.cs
    - Creation of BookShoppingCartMVC\BookShoppingCartMVC\Constants\Roles.cs
    - Adding role to user: await _userManager.AddToRoleAsync(user, Roles.User.ToString()); to BookShoppingCartMVC\BookShoppingCartMVC\Areas\Identity\Pages\Account\Register.cshtml.cs
    - Creation of BookShoppingCartMVC\BookShoppingCartMVC\Data\DbSeeder.cs
    - Add a new service "builder.Services" (line 15) and "DbSeeder" (line 24) to BookShoppingCartMVC\BookShoppingCartMVC\Program.cs
    - Add logic to mark when the user is an admin in BookShoppingCartMVC\BookShoppingCartMVC\Views\Shared\_LoginPartial.cshtml

---
+++ BookShoppingCartMVC/Constants/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShoppingCartMVC.Constants
+{
+    public enum Roles
+    {
+        User = 1 /*If there is no assigned any value to it, it will be a zero.*/,
+        Admin
+    }
+}
---
+++ BookShoppingCartMVC/Program.cs
@@ -0,0 +1,59 @@
+using BookShoppingCartMVC.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+/*builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();*/
+builder.Services
+    .AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+/*
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//This code should only be used one time.
+//Uncomment it when you run the project first time, It will registered an admin.
+using (var scope = app.Services.CreateScope())
+{
+    await DbSeeder.SeedDefaultData(scope.ServiceProvider);
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
