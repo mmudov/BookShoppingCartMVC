d4734bb4579ea3ab215e6ba173b3c7afd01753c6
mmudov <milen.mudov@gmail.com>
2025-01-22 16:40:13 +0200

    Add CSS modifications, add <script> section to BookShoppingCartMVC\BookShoppingCartMVC\Views\Home\Index.cshtml
    Creation of BookShoppingCartMVC\BookShoppingCartMVC\Repositories\CartRepository.cs
    Creation of BookShoppingCartMVC\BookShoppingCartMVC\Repositories\ICartRepository.cs
    Add "ICartRepository", "CartRepository" to BookShoppingCartMVC\BookShoppingCartMVC\Program.cs
    Creation of BookShoppingCartMVC\BookShoppingCartMVC\Controllers\CartController.cs
    CSS modifications of BookShoppingCartMVC\BookShoppingCartMVC\Views\Shared\_LoginPartial.cshtml
    Add <script> section to BookShoppingCartMVC\BookShoppingCartMVC\Views\Shared\_Layout.cshtml
    Creation of BookShoppingCartMVC\BookShoppingCartMVC\Views\Cart\GetUserCart.cshtml

---
+++ BookShoppingCartMVC/Repositories/ICartRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShoppingCartMVC.Repositories
+{
+    public interface ICartRepository
+    {
+        Task<int> AddItem(int bookId, int quantity);
+        Task<int> RemoveItem(int bookId);
+        Task<ShoppingCart> GetUserCart();
+        Task<int> GetCartItemCount(string userId = "");
+        Task<ShoppingCart> GetCart(string userId);
+    }
+}
